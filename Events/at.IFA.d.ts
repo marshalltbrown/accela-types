50eb9fc5621c76607055a6b464035a2a4e4b3c69
Marshall Brown <82731225+marshalltbrown@users.noreply.github.com>
2023-02-13 20:37:21 -0800

    Create at.IFA.d.ts

---
+++ Events/at.IFA.d.ts
@@ -0,0 +1,8 @@
+/// <reference path="at.generic-event-vars.d.ts" />
+
+declare var FeeObjs: undefined
+declare var StatusDate: Date
+declare var CFTOKEN: String
+declare var CFID: String
+declare var projectScriptModel: ProjectScriptModel
+declare var xyz: string
